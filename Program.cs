b89a67dd7498921cf35615cc7ee567f1edb0cd1b
AltafHusain-mca <altaf_mca2k4@yahoo.co.in>
2024-07-01 18:34:47 +0530

    Hello word code line is removed

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using IBM_HashUrl;
-Console.WriteLine("Hello, World!");
 clsHash objHashCode = new clsHash();
 string hashCode=objHashCode.GettingHashCode("https://stackoverflow.com", "tdspu", 5);
 Console.WriteLine("Hash code is :" + hashCode);
