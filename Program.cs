b67b6242fd09117eeb18434733e54b860d16be43
AltafHusain-mca <altaf_mca2k4@yahoo.co.in>
2024-07-01 18:31:33 +0530

    Code has been change

--- Program.cs
+++ Program.cs
@@ -3,5 +3,5 @@ using IBM_HashUrl;
 Console.WriteLine("Hello, World!");
 clsHash objHashCode = new clsHash();
 string hashCode=objHashCode.GettingHashCode("https://stackoverflow.com", "tdspu", 5);
-Console.WriteLine("Hash code is :"+ hashCode);
+Console.WriteLine("Hash code is :" + hashCode);
 Console.ReadLine();
